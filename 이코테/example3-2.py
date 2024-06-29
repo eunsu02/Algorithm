4601f378837b5a28814fa4cd62d99927b391083d
최은수 <Eun@choeeunsuui-noteubug.local>
2024-06-30 01:12:40 +0900

    [이것이 코딩테스트다] 큰 수의 법칙 문제 코드 디벨롭

--- 이코테/example3-2.py
+++ 이코테/example3-2.py
@@ -10,7 +10,6 @@ first = numbers[-1]
 second = numbers[-2]
 
 result = 0
-repeat = 0
 
 start_time = time.time()
 while True:
@@ -26,8 +25,30 @@ while True:
 print(result)
 print("직접 작성한 코드 수행 시간 : ", time.time() - start_time)
 
+# 코드 수정본
+n, m, k = map(int, input().split())
+numbers = list(map(int, input().split()))
+
+numbers.sort()
+first = numbers[-1]
+second = numbers[-2]
+result = 0
+
+몫, 나머지 = divmod(m, k + 1)
+result = 몫 * (first * k + second) + 나머지 * first
+print(result)
+
 
 # 교재 답안
+
+n, m, k = map(int, input().split())
+numbers = list(map(int, input().split()))
+
+numbers.sort()
+first = numbers[-1]
+second = numbers[-2]
+result = 0
+
 start_time = time.time()
 while True:
     for i in range(k):
@@ -40,4 +61,21 @@ while True:
     result += second
     m -= 1
 print(result)
-print("교재 답안 코드 수행 시간 : ", time.time() - start_time)
+print("교재 답안1 코드 수행 시간 : ", time.time() - start_time)
+
+n, m, k = map(int, input().split())
+numbers = list(map(int, input().split()))
+
+numbers.sort()
+first = numbers[-1]
+second = numbers[-2]
+result = 0
+
+start_time = time.time()
+count = int(m / (k + 1)) * k
+count += m % (k + 1)
+
+result += count * first
+result += (m - count) * second
+print(result)
+print("교재 답안2 코드 수행 시간 : ", time.time() - start_time)
