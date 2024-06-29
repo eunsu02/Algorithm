643d696cb31de4316692b595b4a363311d495111
최은수 <Eun@choeeunsuui-noteubug.local>
2024-06-29 23:46:22 +0900

    [이것이 코딩테스트다] 예제3-1

---
+++ 이코테/example3-1.py
@@ -0,0 +1,29 @@
+import time
+
+n = 1260  # 1260원을 돌려받아야한다고 가정한다.
+
+coin_type = [500, 100, 10]
+
+count = 0
+
+start_time = time.time()
+for coin in coin_type:
+    count += int(n / coin)
+    print(n, coin, count)
+    n -= coin * int(n / coin)
+    # print(coin, count, n)
+    if n <= 0:
+        break
+print(count)
+print("첫번째 시도 걸린 시간 : ", time.time() - start_time)
+
+count = 0
+n = 1260
+
+start_time = time.time()
+for coin in coin_type:
+    count += n // coin  # //로 몫을 구할 수 있다.
+    n %= coin  # %로 나머지를 구할 수 있다.
+
+print(count)
+print("두번째 시도 걸린 시간 : ", time.time() - start_time)
