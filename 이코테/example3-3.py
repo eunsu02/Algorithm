643891462a73bd788eadc07e1570d7744c612551
최은수 <Eun@choeeunsuui-noteubug.local>
2024-06-30 13:27:33 +0900

    [이것이 코딩테스트다] 숫자 카드 게임, 1일 될때 까지

---
+++ 이코테/example3-3.py
@@ -0,0 +1,11 @@
+# 숫자 카드 게임
+
+n, m = map(int, input().split())
+
+min_list = []
+
+for i in range(n):
+    num = list(map(int, input().split()))
+    min_list.append(min(num))
+
+print(max(min_list))
