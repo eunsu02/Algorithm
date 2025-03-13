52c12a3620c34fa4f5fc2160390f0e32e87cb828
Eunsu Choi <regulus_leo08@naver.com>
2025-03-13 11:20:24 +0900

    [Silver III] Title: 바이러스, Time: 196 ms, Memory: 17868 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,36 +1,50 @@
-
 import java.util.*;
 
 public class Main {
-    static boolean[] visited;
-    static int count=0;
-    static int[][] arr;
-    static int node,line;
+	
+    public static int[][] node;
+    public static boolean[] visited;
+    public static int M,N;
+    public static int result = 0;
+    
+    public static int bfs(int start) {
+        Queue<Integer> que = new LinkedList<>();
+        visited[start] = true;
+        que.offer(start);
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        node = sc.nextInt();
-        line = sc.nextInt();
-        arr=new int[node+1][node+1];
-        visited = new boolean[node+1];
+        while(!que.isEmpty()) {
+            int temp = que.poll(); //현재 위치 저장
 
-        for(int i=0;i<line;i++){
-            int a=sc.nextInt();
-            int b=sc.nextInt();
-            arr[a][b]=arr[b][a]=1;
-        }
-        dfs(1);
-        System.out.println(count -1);
-
-    }
-
-    public static void dfs(int start){
-        visited[start]= true;
-        count++;
-        for(int i=0;i<=node;i++){
-            if(arr[start][i] == 1 && !visited[i]){
-                dfs(i);
+            for(int i=1; i<=M; i++) {
+                if(node[temp][i] == 1 && !visited[i]){
+                    que.offer(i);
+                    visited[i] = true;
+                    result ++;
+                }
             }
         }
+
+        return result;
     }
+    
+	public static void main(String[] args) throws Exception {
+		
+        Scanner sc = new Scanner(System.in);
+		
+        M = sc.nextInt();
+        N = sc.nextInt();
+ 
+        node = new int[M+1][M+1];
+        visited = new boolean[M+1];
+ 
+        for(int i=0; i<N; i++){
+           int m = sc.nextInt();
+           int n = sc.nextInt();
+           node[m][n] = node[n][m] = 1;
+        }
+        
+        System.out.println(bfs(1));
+	}
+	
+	
 }
