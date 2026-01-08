78a955975455bfa0a7228a4bf6b29a9f2b8b5c24
Eunsu Choi <regulus_leo08@naver.com>
2026-01-08 14:10:47 +0900

    [Silver II] Title: A → B, Time: 132 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static long A,B;
+    static int count;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        A = Long.parseLong(st.nextToken());
+        B = Long.parseLong(st.nextToken());
+        System.out.println(bfs());
+    }
+    public static int bfs(){
+        Queue<Long> q = new LinkedList<>();
+        q.add(A);
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                long temp = q.poll();
+                if(temp==B){
+                    return count+1;
+                }
+                if(temp*2<=B) q.add(temp*2);
+                if(temp*10+1<=B) q.add(temp*10+1);
+            }
+            count++;
+        }
+        return -1;
+    }
+}
