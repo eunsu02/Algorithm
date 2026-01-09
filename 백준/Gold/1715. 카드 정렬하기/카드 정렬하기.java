46794dbd965c60d63714680c5111ba3cd19946aa
Eunsu Choi <regulus_leo08@naver.com>
2026-01-09 13:25:59 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 304 ms, Memory: 24924 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            pq.offer(Integer.parseInt(br.readLine()));
+        }
+
+        long answer = 0;
+
+        while (pq.size() > 1) {
+            int a = pq.poll();
+            int b = pq.poll();
+            int sum = a + b;
+
+            answer += sum;
+            pq.offer(sum);
+        }
+
+        System.out.println(answer);
+    }
+}
